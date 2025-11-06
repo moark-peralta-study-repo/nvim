5fbe51acb3f779f38ef80cd666e56ea4d5e00f7c
moark-peralta-study-repo <markperaltaasd@gmail.com>
2025-11-06 16:13:17 +0800

    setup dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,24 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local keymap = vim.keymap
+
+    dapui.setup()
+
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+
+    --Keymaps
+    keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Debug Toggle Breakpoint" })
+    keymap.set("n", "<leader>ds", dap.continue, { desc = "Debug Start" })
+    keymap.set("n", "<leader>dc", dap.close, { desc = "Debug Close" })
+  end,
+}
